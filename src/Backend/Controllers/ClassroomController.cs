75f4222ac7804c429c0ffff961a36ac3f1a67d4d
Michielfx <m.van.huijstee.mvh@gmail.com>
2021-10-21 14:23:07 +0200

    Updated backend startup

---
+++ src/Backend/Controllers/ClassroomController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KindergartenApi.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ClassroomController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<int>> GetClassrooms()
+    {
+        return new[] { 101, 102, 103, 202, 203, 204 };
+    }
+}
--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -2,21 +2,27 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new() { Title = "KindergartenApi", Version = "v1" }); });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+// if (app.Environment.IsDevelopment())
+// {
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "KindergartenApi v1"));
-}
+// }
 
-app.UseHttpsRedirection();
+// app.UseRouting();
+
+app.UseCors(m =>
+{
+    // m.WithOrigins("https://admin.mineapple.net", "http://localhost:3001", "https://localhost:3001", "https://192.168.1.2:3001",
+    //     "http://192.168.1.2:3001");
+    m.AllowAnyOrigin();
+    m.AllowAnyMethod();
+    m.AllowAnyHeader();
+});
 
 app.UseAuthorization();
 
