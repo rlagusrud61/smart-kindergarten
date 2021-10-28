fc3be4deb7d5e1f65b4a366234fbad3251d2661e
Hyeon Kyeong Kim <kimhk@inter-actief.net>
2021-10-28 17:18:33 +0200

    search functionality and showing student profile

--- src/Frontend/src/components/layout/StudentProfile.tsx
+++ src/Frontend/src/components/layout/StudentProfile.tsx
@@ -1,30 +1,33 @@
 import { StudentHistory } from "./StudentHistory";
 
-export function StudentProfile() {
-
+interface StudentProfileProps{
+    student: string;
+}
+export function StudentProfile({student}:StudentProfileProps) {
     return (
         <div className="relative p-9">
 
             {/* Image and name of student  */}
-            <div className="p-3 grid grid-cols-3">
-                <img className="col-span-1 object-contain transform w-32 rounded-full" alt='kid' src="img/kidlmao.jpg" />
-                <div className="py-5 table-cell row-span-2 col-span-2">
-                    <h1 className="align-middle font-bold text-4xl">X Æ A-12</h1>
-                    <br></br>
-                    <h3 className="align-middle text-2xl">Class 1</h3>
+            <div className="bg-white shadow-lg rounded-xl p-3">
+                <div className="p-4 grid grid-cols-3">
+                    <img className="col-span-1 object-contain transform w-32 rounded-full" alt='kid' src="img/kidlmao.jpg" />
+                    <div className="py-5 table-cell col-span-2">
+                        <h1 className="text-gray-700 align-middle font-bold text-3xl">{student}</h1>
+                        <br></br>
+                        <h3 className="align-middle text-xl">Age : 5</h3>
+                    </div>
                 </div>
+
+                <div className="p-4 grid grid-cols-3">
+                <img className="col-span-1 transform w-24 object-contain px-5" src="img/heart.png"></img>
+                    <h1 className="col-span-2 align-middle text-2xl"> 90 BPM</h1>
+
+                </div>
+                {/* Heart Rate */}
+
+
             </div>
-
-            {/* Heart Rate */}
-
-            <div className="p-8 grid grid-cols-3">
-            <svg className="col-span-1" xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 20 20">
-                <path d="M14.75 1A5.24 5.24 0 0 0 10 4 5.24 5.24 0 0 0 0 6.25C0 11.75 10 19 10 19s10-7.25 10-12.75A5.25 5.25 0 0 0 14.75 1z" />
-            </svg>
-            <h1 className="col-span-2 py-4 text-2xl"> 90 BPM</h1>
-            </div>
-
-
+            <br></br>
             <StudentHistory></StudentHistory>
         </div>
     );
--- src/Frontend/src/components/pages/Home.tsx
+++ src/Frontend/src/components/pages/Home.tsx
@@ -1,10 +1,23 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import logo from '../../img/logo.svg';
 import '../../styles/tailwind.css'
 import { ClassroomClient } from "../../clients/rest/ApiClient";
 import { StudentProfile } from './../layout/StudentProfile';
 import { StudentList } from './../layout/StudentList';
+
 function Home() {
+
+  const [selectedStudent, setSelectedStudent] = useState <number>();
+
+    const students = [
+        "Jan",
+        "Michiel",
+        "Jeroen",
+        "Aachi",
+        "Niels",
+        "Hyunk",
+        "X Æ A-12",
+    ];
 
   useEffect(() => {
     console.log("aaaaa")
@@ -14,33 +27,25 @@ function Home() {
   })
 
   return (
-
-
     <main className="relative h-screen">
-
-
-
-      
       <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-        {/* <!-- Replace with your content --> */}
         <div className="grid grid-cols-3 gap-20 px-4 py-6 sm:px-0">
           <div className="...">
 
-            <h1 className="py-10 text-center text-3xl font-bold text-gray-900">Students</h1>
-            <div className="bg-white shadow-lg rounded-lg h-screen">
-             <StudentList></StudentList> 
+            <h1 className="py-10 text-center text-4xl font-bold text-gray-700">Students</h1>
+            <div className="bg-gray-100 shadow-lg rounded-lg h-screen">
+             <StudentList students={students} setSelectedStudent={setSelectedStudent}/> 
             </div>
           </div>
           <div className="col-span-2 ...">
 
-            <h1 className="py-10 text-center text-3xl font-bold text-gray-900">Student Profile</h1>
-            <div className="overflow-auto border-4 border-dashed border-gray-200 rounded-lg h-screen">
-              <StudentProfile></StudentProfile>
+            <h1 className="py-10 text-center text-4xl font-bold text-gray-700">Student Profile</h1>
+            <div className="overflow-auto bg-gray-100 shadow-lg rounded-lg h-screen">
+              <StudentProfile student={students[selectedStudent ?? 0]}/>
             </div>
 
           </div>
         </div>
-        {/* <!-- /End replace --> */}
       </div>
 
     </main>
