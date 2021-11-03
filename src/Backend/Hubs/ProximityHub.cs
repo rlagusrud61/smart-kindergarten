b9be1f986fea9117cb3948ad5c98563c94d67cfd
Michielfx <m.van.huijstee.mvh@gmail.com>
2021-11-03 21:39:40 +0100

    Updated api endpoints and processing

---
+++ src/Backend/Hubs/ProximityHub.cs
@@ -0,0 +1,18 @@
+﻿using KindergartenApi.Models.DB;
+using KindergartenApi.Models.DTO;
+using Microsoft.AspNetCore.SignalR;
+
+namespace KindergartenApi.Hubs;
+
+public interface IProximityHub
+{
+    Task UpdateDeviceProximity(string ownerHardwareAddress, IEnumerable<NearbyBluetooth> nearbyDevices);
+}
+
+public class ProximityHub : Hub<IProximityHub>
+{
+    public async Task SendUpdatedGroups(string ownerHardwareAddress, IEnumerable<NearbyBluetooth> nearbyDevices)
+    {
+        await Clients.All.UpdateDeviceProximity(ownerHardwareAddress, nearbyDevices);
+    }
+}
