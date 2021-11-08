1336eaf1c72fa0275bda0a0c1af083c608a43b65
Michielfx <m.van.huijstee.mvh@gmail.com>
2021-11-08 07:40:29 +0100

    Minor theme fix

--- src/Frontend/src/components/theme/Background.tsx
+++ src/Frontend/src/components/theme/Background.tsx
@@ -1,7 +1,7 @@
 export const Background = ({children} :any) => {
     return (
         // Remove transition-all to disable the background color transition.
-        <div className="bg-white dark:bg-gray-800 transition-all min-h-screen page-content">
+        <div className="bg-white dark:bg-gray-800 transition-all min-h-screen h-full page-content">
         {children}
         </div>
     )
